af4b11ba5feefab667ffd8c20a1818eaf0f0b711
Drew Barontini <drewbarontini@gmail.com>
2016-12-17 11:28:11 -0500

    Create Gulp build task to minify

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,8 @@
 
 var gulp = require('gulp');
 var scss = require('gulp-scss');
+var cleanCSS = require('gulp-clean-css');
+var rename = require('gulp-rename');
 
 // -------------------------------------
 //   Task: Compile
@@ -18,5 +20,16 @@ var scss = require('gulp-scss');
 gulp.task('compile', function() {
   gulp.src('application.scss')
     .pipe(scss())
+    .pipe(gulp.dest('dist'));
+});
+
+// -------------------------------------
+//   Task: Build
+// -------------------------------------
+
+gulp.task('build', function() {
+  gulp.src('dist/application.css')
+    .pipe(rename('application.min.css'))
+    .pipe(cleanCSS())
     .pipe(gulp.dest('dist'));
 });
