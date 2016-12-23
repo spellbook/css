a267d36dd6f791dae760a6de5827728c3498c9c4
Drew Barontini <drewbarontini@gmail.com>
2016-12-23 09:29:47 -0500

    Add gulp-autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,7 @@
 // -------------------------------------
 
 var gulp = require('gulp');
+var autoprefixer = require('gulp-autoprefixer');
 var scss = require('gulp-scss');
 var cleanCSS = require('gulp-clean-css');
 var rename = require('gulp-rename');
@@ -20,6 +21,10 @@ var rename = require('gulp-rename');
 gulp.task('compile', function() {
   gulp.src('application.scss')
     .pipe(scss())
+    .pipe(autoprefixer({
+      browsers : ['last 2 versions'],
+      cascade : false
+    }))
     .pipe(rename('spellbook.css'))
     .pipe(gulp.dest('dist'));
 });
